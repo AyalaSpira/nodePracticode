a1f59fb1334bf209035bad5c22ac01b9a9489d83
Your Name <you@example.com>
2025-02-05 18:23:54 +0200

    הודעת קומיט

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import renderApi from '@api/render-api';
 
-const express = require('express');
-const app = express();
+import express from 'express';
 
-const PORT = process.env.PORT || 3000; // Render יקבע את הפורט
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 renderApi.auth('rnd_aQ4FYLkhTfmAShXhDVCSfjlcz5xV');
 renderApi.listServices({includePreviews: 'true', limit: '20'})
